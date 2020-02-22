d06ca764f03f988b82d993fdc9c8a439f2e01d92
mitsu733 <tubbbbbu@yahoo.co.jp>
2020-02-22 23:05:08 +0000

    Revert "bookers2-debug"
    This reverts commit 014e30f69458d4d7f481a42a7f7614393664b8bb.

--- app/models/book.rb
+++
@@ -1,8 +0,0 @@
-class Book < ApplicationRecord
-	belongs_to :user
-	#バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
-	#presence trueは空欄の場合を意味する。
-	validates :title, presence: true
-	validates :body, presence: true
-	validates :body, length: { maximum: 200 }
-end
--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  root 'home#top'
-  get 'home/about', to: "home#about"
-  devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
-
-end
